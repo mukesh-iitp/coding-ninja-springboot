6ecb554bbc071dd5a9a73867204a2587c86c2811
mukesh-iitp <146891811+mukesh-iitp@users.noreply.github.com>
2024-10-27 02:54:04 +0530

    Add files via upload

---
+++ CarService_part4/src/main/java/com/example/CarService/web/attendentController.java
@@ -0,0 +1,26 @@
+package com.example.CarService.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ 1. getAttendent method returns string "attendent" for Attendent page.
+ 2. Also, it accepts attendentId of type String and object of type ModelMap as argument.
+ 3. Use @PathVariable annotation accordingly.
+**/
+
+@Controller
+public class attendentController {
+    
+  @GetMapping("/attendent/{attendentId}")
+    public String getAttendent(@PathVariable("attendentId") String attendentId, ModelMap map){
+      //Write your Logic here
+	  map.addAttribute("attendentId", attendentId);
+      map.addAttribute("name", "TEST 123");
+      map.addAttribute("work","Engine,BodyShop");
+	  return "attendent";
+    }
+
+}
---
+++ CarService_part4/src/main/webapp/WEB-INF/jsp/attendent.jsp
@@ -0,0 +1,8 @@
+<html>
+<h1>This is the Attendent Page</h1>
+<div>
+    Attendent Name:<%=request.getAttribute("name") %>
+    <br>
+    Speciality: <%=request.getAttribute("work") %>
+</div>
+</html>
