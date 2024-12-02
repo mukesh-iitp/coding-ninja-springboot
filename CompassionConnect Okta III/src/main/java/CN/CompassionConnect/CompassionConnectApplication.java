19b5b6c0fb923698b4f1ca65232db2503749339b
Mukesh Kr <146891811+mukesh-iitp@users.noreply.github.com>
2024-12-03 02:43:59 +0530

    CompassionConnect Okta III
    CompassionConnect Okta III

---
+++ CompassionConnect Okta III/src/main/java/CN/CompassionConnect/CompassionConnectApplication.java
@@ -0,0 +1,13 @@
+package CN.CompassionConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassionConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassionConnectApplication.class, args);
+	}
+
+}
