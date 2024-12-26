c1bf8c116347a94ecc59272986c0ae9b0b333780
Mukesh Kr <146891811+mukesh-iitp@users.noreply.github.com>
2024-12-27 01:31:48 +0530

    Add files via upload

---
+++ ConnectMaster/BillingService/src/main/java/Telecom/BillingService/exception/InternalServerError.java
@@ -0,0 +1,24 @@
+package Telecom.BillingService.exception;
+
+import java.io.Serial;
+
+public class InternalServerError extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String message;
+
+    public InternalServerError() {
+        super();
+    }
+
+    public InternalServerError(String message) {
+        super();
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
